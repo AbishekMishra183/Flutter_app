e504102cda5a30975df0f8264bc1b2db5974212c
abishek Mishra <abishekmishra183@gmail.com>
2025-07-10 08:35:39 +0545

    add features folder and code

---
+++ lib/Features/Auth/VIews/registation_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegistationPage extends StatelessWidget {
+  const RegistationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
